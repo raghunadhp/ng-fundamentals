72556578a5675d94cd1d775e94aad1da3b038dca
Raghu <praghu91@gmail.com>
2019-03-14 07:50:51 +0530

    [ - ]Module 5 Completed.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ToastrModule } from 'ngx-toastr';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
 
 @NgModule({
     imports: [
-    BrowserModule
+    BrowserModule,
+    ToastrModule.forRoot()
   ],
   declarations: [
     EventsAppComponent,
@@ -16,6 +19,7 @@ import { NavBarComponent } from './nav/navbar.component';
     EventThumbnailComponent,
     NavBarComponent
   ],
+  providers: [EventService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
---
+++ src/app/common/toastr.service.ts
@@ -0,0 +1,6 @@
+// import { Injectable } from '@angular/core';
+
+
+// @Injectable()
+// export class ToastrService {}
+// already toastr service is available with the latest library. no need to create explicitly.
